9b2fd689d1f4baaa837a1562b757a8b44297ead7
Tiger Soldier <tigersoldi@gmail.com>
2010-05-18 06:58:23 +0000

    CHANGE: Remove gtkprivate.h dependency in OSD Window

--- src/ol_osd_window.c
+++ src/ol_osd_window.c
@@ -16,7 +16,6 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-#include <gtk/gtkprivate.h>
 #include "ol_osd_window.h"
 #include "ol_utils.h"
 #include "ol_debug.h"
