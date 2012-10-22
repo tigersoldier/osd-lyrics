ad3a871aabedf29712454c895273ba5935e02ef6
Tiger Soldier <tigersoldi@gmail.com>
2012-10-21 23:08:30 -0700

    autogen: support generate from subdirs

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+DIR=`dirname $0`
+
 args=$@
 
 if [ -e /usr/bin/python2 ]; then
     args="$args PYTHON=/usr/bin/python2"
 fi
-autoreconf --install --force
-./configure $args
+autoreconf --install --force $DIR
+$DIR/configure $args
